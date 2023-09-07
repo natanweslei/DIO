c225896188765c73520db6a628d3252965e1c1a9
Natan <natanwesleii@gmail.com>
2023-09-07 18:57:36 -0300

    Commit
    concluído novo desafio

--- SistemaContador/src/Desafio/MonitoramentoLogsAWS.java
+++ SistemaContador/src/Desafio/MonitoramentoLogsAWS.java
@@ -7,19 +7,56 @@ import java.util.Scanner;
 public class MonitoramentoLogsAWS {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int totalEventos = 0;
+        Map<String, Integer> eventosPorServico = new HashMap<>();
+        String maiorServico = "";
+        String menorServico = "";
+
+
         int quantidadeLogs = scanner.nextInt();
         scanner.nextLine(); // Limpar o buffer do scanner
 
-        Map<String, Long> eventosPorServico = new HashMap<>();
+
+        //TODO:  realize a análise dos logs e retorne os seus resultados
+        // Itera sobre os logs e realiza a análise
         for (int i = 0; i < quantidadeLogs; i++) {
-            String[] parts = scanner.nextLine().split(",");
-            String servico = parts[1];
-            //TODO: Utilize o mapa para registrar/incrementar o serviço em questão.
+            String log = scanner.nextLine(); // Lê uma linha de log
+            String[] logInfo = log.split(","); // Divide a linha em informações usando vírgula como separador
+
+            if (logInfo.length != 3) {
+                continue; // Ignora logs com informações incompletas
+            }
+            String servico = logInfo[1].trim(); // Obtém o nome do serviço a partir das informações
+            eventosPorServico.put(servico, eventosPorServico.getOrDefault(servico, 0) + 1); // Atualiza a contagem de eventos para o serviço
+            totalEventos++; // Incrementa o total de eventos
         }
 
-        //TODO: Identifique no mapa os serviços com maior e menor quantidade de logs.
-        //      Dica: Utilize Java Streams para tornar essa tarefa mais simples.
+        // Encontra o serviço com maior quantidade de registros
+        int maxEventos = -1;
+        for (Map.Entry<String, Integer> entry : eventosPorServico.entrySet()) {
+            if (entry.getValue() >= maxEventos) {
+                maxEventos = entry.getValue();
+                maiorServico = entry.getKey();
+            }
+        }
 
-        //TODO: Imprima a saída no padrão definido no enunciado deste desafio.
+        // Encontra o serviço com menor quantidade de registros
+        int minEventos = Integer.MAX_VALUE;
+        for (Map.Entry<String, Integer> entry : eventosPorServico.entrySet()) {
+            if (entry.getValue() < minEventos) {
+                minEventos = entry.getValue();
+                menorServico = entry.getKey();
+            }
+        }
+
+        // Exibe os resultados da análise
+        System.out.println("Eventos por servico:");
+        for (Map.Entry<String, Integer> entry : eventosPorServico.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+
+        System.out.println("Maior:" + maiorServico);
+        System.out.println("Menor:" + menorServico);
+        scanner.close();
     }
 }
